9ab6fc0abe9bcdbe31f725bd6efe0cee70c32ee5
angelafeliciaa <angela_felicia@yahoo.com>
2024-03-09 14:14:58 -0800

    feat: home page

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import LottieView from 'lottie-react-native'; // Import LottieView
 
-export default function App() {
+// Define the Home component
+const Home = ({ navigation }) => {
+  // Sample function to handle button press
+  const handleButtonPress = () => {
+    // Navigate to another screen or perform any desired action
+    // For example, you can use navigation.navigate('OtherScreen');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+      style={styles.container}
+      colors={["#86F1F9", "#FDD0ED"]}
+      start={{ x: 0.1, y: 0.1 }}
+      end={{ x: 0.1, y: 1}}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>Love Alarm</Text>
+        {/* Include the LottieView component */}
+        <LottieView
+          source={require("./assets/heartAnimation.json")} // Provide the correct path
+          autoPlay
+          loop
+          style={styles.animation}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
+          <Text style={styles.buttonText}>Start Matching</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
-}
+};
 
+// Define styles using StyleSheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: 'white', // Set text color to white for better visibility
+  },
+  button: {
+    backgroundColor: '#F87B92',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  animation: {
+    width: 200, // Set the width and height as needed
+    height: 200,
   },
 });
+
+// Export the Home component
+export default Home;
