995e5df5231c2741e84cf093bc53bc0de70392f9
Yuri Yasui <yuriyasui012@gmail.com>
2024-03-09 16:59:52 -0800

    mogoodb works!!!

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 // not sure --------------------------
 const app = express();
 const PORT = process.env.PORT || 3000;
 // -----------------------------------
-
+console.log(process.env)
+console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
